a936e512689268cbd358dd6fac929006332cd3b3
nogueirarcz <68076593+nogueirarcz@users.noreply.github.com>
2022-10-14 22:24:21 -0300

    Relação de Composição

---
+++ section-4/110/classes.py
@@ -0,0 +1,27 @@
+# Composição
+class Cliente:
+
+    def __init__(self, nome, idade):
+
+        self.nome = nome
+        self.idade = idade
+        self.enderecos = []
+
+    def inserir_endereco(self, cidade, estado):
+
+        self.enderecos.append(Endereco(cidade, estado))
+
+    def lista_enderecos(self):
+
+        for endereco in self.enderecos:
+
+            print(endereco.cidade, endereco.estado)
+
+
+class Endereco:
+
+    def __init__(self, cidade, estado):
+
+        self.cidade = cidade
+        self.estado = estado
+    
---
+++ section-4/110/main.py
@@ -0,0 +1,19 @@
+from classes import Cliente
+
+cliente1 = Cliente('Luiz', 32)
+cliente1.inserir_endereco('Belo Horizonte', 'MG')
+
+cliente2 = Cliente('Maria', 55)
+cliente2.inserir_endereco('Salvador', 'BA')
+cliente2.inserir_endereco('Rio de Janeiro', 'RJ')
+
+cliente3 = Cliente('João', 19)
+cliente3.inserir_endereco('São Paulo', 'SP')
+
+print(cliente1.nome)
+cliente1.lista_enderecos()
+
+print('')
+
+print(cliente2.nome)
+cliente2.lista_enderecos()
